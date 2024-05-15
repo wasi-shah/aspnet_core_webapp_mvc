a39a66a05d40a3d42e88a43109576163887dfa72
wasi-shah <51749920+wasi-shah@users.noreply.github.com>
2024-05-15 14:09:12 +0100

    Update

--- obj/Release/net8.0/aspnet-core-webapp-mvc.AssemblyInfo.cs
+++ obj/Release/net8.0/aspnet-core-webapp-mvc.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspnet-core-webapp-mvc")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc72a933aef467dc991ed67c7b59349689e760b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspnet-core-webapp-mvc")]
